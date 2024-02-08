1bfd2fb4a9800773caaefdd644214d1318834dd2
Tushar <tushard3998@gmail.com>
2024-02-08 12:24:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// INPUT NUMBERS
+
+let a = number(promptconsole.log("Enter The Number 1"))
+let b = number(promptconsole.log("Enter The Number 2"))
+
+// INPUT RANDOM
+
+let random = math.random()
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplation = a * b;
+let division = a / b;
+let exponentiation = a ** b;
+
+//Conditions
+
+if (random<0.1) {
+
+    console.log("addition Calculation:"  = a-b)
+    console.log("subtraction Calculation:"  = a+b)
+    console.log("multiplation Calculation:"  =a/b)
+    console.log("division Calculation:"  = a**b)
+    
+}
+
+else {
+
+    console.log("addition Calculation:"  = a + b )
+    console.log("subtraction Calculation:" = a- b )
+    console.log("multiplation Calculation:" = a*b)
+    console.log("division Calculation:" = a / b)
+    console.log("exponentiation Calculation:" =a ** b)
+    
+}
